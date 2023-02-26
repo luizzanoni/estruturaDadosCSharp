d31beafae7e56b527085ebd1df3e876675d61e83
Luiz Gustavo Zanoni <luizzanoni47@gmail.com>
2023-02-25 23:39:19 -0300

    start estudos

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# estruturaDadosCSharp
+# Estrutura de Dados C#
+
+Boas práticas em estruturas de dados utilizando C#
+
+Além de aprendizado, codado novas soluções para alguns bugs que foram listados na Hackathon mundial!
+
+(https://www.youtube.com/watch?v=byVgPI42YD0) utilizado este vídeo
---
+++ array.cs
@@ -0,0 +1,29 @@
+// array
+
+// Value => 7, 11, 6, 55, 98, 45, 16, 96, 46
+// Index => 0, 1, 2, 3, 4, 5 6, 7, 8 (são de 8 posições caso colocar mais ele retorna erro!)
+
+using System;
+
+namespace Dotnet.Array
+{
+    public static class ArrayDS
+    {
+        public static int[] ReserveArray(int[] array)
+        {
+            //Declaro aqui um novo array
+            int[] reverseArray = new int[array.Lenght];
+
+            //Variável para controlar posições do array
+            int position = 0;
+
+            for (int i = array.Lenght; i<= 0; i--)
+            {
+                //populando um novo array
+                reverseArray[position] = array[i];
+            }
+
+            return reverseArray;
+        }
+    }
+}
